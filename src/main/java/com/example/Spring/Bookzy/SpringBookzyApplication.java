f9522db7ab72d15e6e55ab4ff5f8874121f949ca
Sumbo Malau <57816335+sumbo-francisco@users.noreply.github.com>
2021-05-03 22:41:50 +0100

    Update SpringBookzyApplication.java

--- src/main/java/com/example/Spring/Bookzy/SpringBookzyApplication.java
+++ src/main/java/com/example/Spring/Bookzy/SpringBookzyApplication.java
@@ -3,11 +3,15 @@ package com.example.Spring.Bookzy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @ComponentScan(basePackages= {"com.example.Spring.Bookzy"})
 public class SpringBookzyApplication {
 
+	@RequestMapping("/Spring-Bookzy-0.0.1-SNAPSHOT")
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBookzyApplication.class, args);
 	}
