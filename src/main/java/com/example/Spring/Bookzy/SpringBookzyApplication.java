c10eca866f2e4196935f4f629e06b1fa3470f545
Sumbo Malau <sumbo.francisco@gmail.com>
2021-05-03 02:21:30 +0100

    Thi is a second commit to solve the issue with whitelabel Error Page

--- src/main/java/com/example/Spring/Bookzy/SpringBookzyApplication.java
+++ src/main/java/com/example/Spring/Bookzy/SpringBookzyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages= {"com.technicalkeeda"})
+@ComponentScan(basePackages= {"com.example.Spring.Bookzy"})
 public class SpringBookzyApplication {
 
 	public static void main(String[] args) {
