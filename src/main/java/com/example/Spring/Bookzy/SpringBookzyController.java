be2e6d49dcd9d059f5c2fb572391fb341745646f
Sumbo Malau <57816335+sumbo-francisco@users.noreply.github.com>
2021-05-03 23:44:36 +0100

    Update SpringBookzyController.java

--- src/main/java/com/example/Spring/Bookzy/SpringBookzyController.java
+++ src/main/java/com/example/Spring/Bookzy/SpringBookzyController.java
@@ -1,12 +1,14 @@
 package com.example.Spring.Bookzy.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class SpringBookzyController {
   
-      @RequestMapping("/Spring-Bookzy-0.0.1-SNAPSHOT")
+      @GetMapping("/Spring-Bookzy-0.0.1-SNAPSHOT")
       public String index() {
         return "Spring Boot Example!!";
       }
